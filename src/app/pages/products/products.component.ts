7b9b74a5b6a4bbcf84835406145bd9111b9101a4
amarGhandour <30914748+amarGhandour@users.noreply.github.com>
2023-03-27 16:06:27 +0200

    delete product

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -3,6 +3,9 @@ import {IProduct} from "@/interfaces/iproduct";
 import {ProductsService} from "@services/products.service";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatPaginator} from "@angular/material/paginator";
+import {ConfirmDialogComponent, ConfirmDialogModel} from "@components/confirm-dialog/confirm-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-products',
@@ -13,12 +16,13 @@ export class ProductsComponent implements OnInit {
 
   products: IProduct[] = [];
 
+  confirmRes: boolean;
   public dataSource: MatTableDataSource<IProduct>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   displayedColumns: string[] = ['image', 'title', 'price', 'category', 'rating', 'actions'];
 
-  constructor(private productsService: ProductsService,) {
+  constructor(private productsService: ProductsService, private dialog: MatDialog, private toaster: ToastrService) {
 
   }
 
@@ -43,7 +47,33 @@ export class ProductsComponent implements OnInit {
     this.productsService.getAllProducts().subscribe(observer);
   }
 
-  deleteCategory(_id: any) {
+  deleteProduct(id: any) {
+    const message = `Are you sure you want to do this?`;
 
+    const dialogData = new ConfirmDialogModel("Confirm Action", message);
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: "400px",
+      data: dialogData
+    });
+
+    dialogRef.afterClosed().subscribe(dialogResult => {
+      this.confirmRes = dialogResult;
+      if (this.confirmRes) {
+        const observer = {
+          next: (result) => {
+            this.toaster.success('Product successfully Deleted.');
+            this.products = this.products.filter((item) => item._id != id);
+            this.dataSource = new MatTableDataSource(this.products);
+            this.dataSource.paginator = this.paginator;
+          },
+          error: (err) => {
+            console.log(err)
+          }
+        }
+        this.productsService.deleteProduct(id).subscribe(observer);
+      }
+    });
   }
+
 }
